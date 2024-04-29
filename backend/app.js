816a6e53f3da3885acbea612e14eea28aac7e4ca
Adithya <adithyahebbar32@gmail.com>
2024-04-29 19:46:56 +0530

    fix: Set request limit

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 const allowedOrigins = ['http://localhost:5173', 'https://codemancers-cart.netlify.app'];
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 app.use(helmet());
