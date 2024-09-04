1f35a662c3f32abe908af8a20f0ac8a84c54d18f
Adithya-Adi <adithyahebbar32@gmail.com>
2024-09-04 23:16:48 +0530

    commit message

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -7,7 +7,7 @@ const NotFoundPage = () : ReactElement => {
   return (
     <div style={{ textAlign: 'center' }}>
       <img src={notFoundImage} alt='404 Not Found' style={{ width: '100%', maxWidth: '700px', margin: 'auto' }} />
-      <p><Button component={Link} to='/' variant='contained' color='primary'>Home--</Button></p>
+      <p><Button component={Linkasdasdas} to='/' variant='contained' color='primary'>Home--</Button></p>
     </div>
   );
 }
